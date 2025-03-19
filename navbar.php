ae2af24bb8f6ee46c9699670f5713f5f3af903cf
JJX <10248961+jjx-@users.noreply.github.com>
2025-03-19 18:54:31 +0000

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,26 +1,20 @@
 <!--navbar-->
 <!--desktop mode-->
 <div class="navbar desktop" id="navbarDesktop">
-	<div class="button">
-		<a href="https://adsb.chaos-consulting.de" target="blank">Map</a>
+	<div class="button" onclick="centeroverlayShow('about')">
+		About
 	</div>
-	<div class="button">
-		<a href="https://chaos-consulting.de" target="blank">Chaos-consulting.de</a>
-	</div>
-	<div class="button">
-		<a href="https://github.com/chaos-consulting/adsberry" target="blank">Build your own!</a>
-	</div>
-	<div class="button">
-		<a href="https://chaos-consulting.de/impressum.php" target="blank">Impressum</a>
-	</div>
-	<div class="button">
-		<a href="about.php">About</a>
-	</div>
+	<a class="button" href="https://github.com/sdrmap/docs/wiki" target="blank">
+		Feed us
+	</a>
+	<a class="button" href="https://sdrmap.org/impressum.php" target="blank">
+		Impressum
+	</a>
 </div>
 
 <!--mobile mode-->
 <div class="navbar mobile" id="navbarMobile">
 	<i class="fas fa-bars" onclick="menueToggle()"></i>
-	<a href="https://chaos-consulting.de/impressum.php" target="blank">Impressum</a>
+	<a href="https://sdrmap.org/impressum.php" target="blank">Impressum</a>
 	<i class="fas fa-window-close" id="mobileClose" onclick="menueHide(); sidebarHide();"></i>
 </div>
