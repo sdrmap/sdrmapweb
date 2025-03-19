7899dfd279e6893927b1f6d9d6d1eb26be5efe28
JJX <10248961+jjx-@users.noreply.github.com>
2025-03-19 18:55:13 +0000

    Add files via upload

--- js/filter.js
+++ js/filter.js
@@ -9,38 +9,87 @@ function filterInit(){
 
 	function stfu(data) {
 		Object.values(data).forEach(function acs(s){
-			if(s.positions > 0){
-				document.getElementById("filterStationList").innerHTML += '<div id="filterStation' + s.name + '" onclick="filterStationSet(\'' + s.name + '\')">' + s.name + '</div>';
-			}
+			document.getElementById("filterStationList").innerHTML += '<div id="filterStation' + s.name + '" onclick="filterStationSet(\'' + s.name + '\')">' + s.name + '</div>';
 		})
 	}
 
-	//Altitude
-	var alts = [1, 1000, 2000, 3000, 4500, 6000, 7500, 9000, 10500, 12000, 13500];
-	alts.forEach(function altinit(a){
-		var alt = a - 1;
-		
-		document.getElementById("filterAltitudeList").innerHTML += '<td style="background-color:' + altitudeToColor(alt) + '" title="0 - ' + a + '" onclick="filterAltitudeSet(\'' + a + '\')" class="filterAltitude" id="filterAltitude' + a + '">X</td>';
-	});
-	document.getElementById("filterAltitudeList").innerHTML += '<td style="background-color:' + altitudeToColor(13501) + '" title="13.500+" onclick="filterAltitudeSet(999999)" class="filterAltitudeActive" id="filterAltitude999999">x</td>';
+	filterUpdate();
 
-	//Airline
-/*	Object.entries(liveStats.models).forEach(function(model){
-		statsModelsData.push({"model":model[0], "count":model[1], "rel":model[1]/liveStats.total*100});
-	});
-	statsModelsTable.replaceData(statsModelsData);
-
-	var statsAirlinesData = [];
-	Object.entries(liveStats.airlines).forEach(function(airline){
-		statsAirlinesData.push({"airline":airline[0], "count":airline[1], "rel":airline[1]/liveStats.total*100});
-	});*/
-	//statsCalc();
+	filterSpeedApply(0, 2000);
 };
+
+var filterAltitudeSliderSpace = 2000;
+
+function filterAltitudeInput(){
+	filterAltitudeApply(filterAltitudeSliderMin.value, filterAltitudeSliderMax.value)
+}
+
+function filterAltitudeSliderMinInput(){
+	if(parseInt(document.getElementById("filterAltitudeSliderMax").value) - parseInt(document.getElementById("filterAltitudeSliderMin").value) <= filterAltitudeSliderSpace){
+		document.getElementById("filterAltitudeSliderMin").value = parseInt(document.getElementById("filterAltitudeSliderMax").value) - filterAltitudeSliderSpace;
+	}
+	filterAltitudeInput();
+}
+function filterAltitudeSliderMaxInput(){
+	if(parseInt(document.getElementById("filterAltitudeSliderMax").value) - parseInt(document.getElementById("filterAltitudeSliderMin").value) <= filterAltitudeSliderSpace){
+		document.getElementById("filterAltitudeSliderMax").value = parseInt(document.getElementById("filterAltitudeSliderMin").value) + filterAltitudeSliderSpace;
+	}
+	filterAltitudeInput();
+}
+
+function filterAltitudeApply(sMin, sMax){
+	filterAltitudeSliderMin.value = sMin;
+	filterAltitudeSliderMax.value = sMax;
+	document.getElementById("filterAltitudeGauge").innerHTML = ftToM(sMin) + ' - ' + ftToM(sMax) + 'm / ' + sMin + ' - ' + sMax + 'ft';
+	filterAltitudeMin = sMin;
+	filterAltitudeMax = sMax;
+	filterAltitudeSliderMinPerc = sMin / 120000 * 100;
+	filterAltitudeSliderMaxPerc = sMax / 120000 * 100;
+	filterUpdate();
+}
+
+var filterSpeedSliderSpace = 10;
+
+function  filterSpeedSet(s){
+	if(s == 'all'){
+		filterSpeedApply(0, 2000);
+	}
+}
+
+function filterSpeedInput(){
+	filterSpeedApply(filterSpeedSliderMin.value, filterSpeedSliderMax.value)
+}
+
+function filterSpeedApply(sMin, sMax){
+	filterSpeedSliderMin.value = sMin;
+	filterSpeedSliderMax.value = sMax;
+	document.getElementById("filterSpeedGauge").innerHTML = sMin + ' - ' + sMax + 'km/h';
+	filterSpeedMin = sMin;
+	filterSpeedMax = sMax;
+	filterSpeedSliderMinPerc = sMin / 2000 * 100;
+	filterSpeedSliderMaxPerc = sMax / 2000 * 100;
+	document.getElementById("filterSpeedTrack").style.background = `linear-gradient(to right, #393f4cd1 ${filterSpeedSliderMinPerc}%, orange ${filterSpeedSliderMinPerc}% , orange ${filterSpeedSliderMaxPerc}%, #393f4cd1 ${filterSpeedSliderMaxPerc}%)`;
+	filterUpdate();
+}
+
+function filterSpeedSliderMinInput(){
+	if(parseInt(document.getElementById("filterSpeedSliderMax").value) - parseInt(document.getElementById("filterSpeedSliderMin").value) <= filterSpeedSliderSpace){
+		document.getElementById("filterSpeedSliderMin").value = parseInt(document.getElementById("filterSpeedSliderMax").value) - filterSpeedSliderSpace;
+	}
+	filterSpeedInput();
+}
+function filterSpeedSliderMaxInput(){
+	if(parseInt(document.getElementById("filterSpeedSliderMax").value) - parseInt(document.getElementById("filterSpeedSliderMin").value) <= filterSpeedSliderSpace){
+		document.getElementById("filterSpeedSliderMax").value = parseInt(document.getElementById("filterSpeedSliderMin").value) + filterSpeedSliderSpace;
+	}
+	filterSpeedInput();
+}
 
 function filterTypeSet(t){
 	document.getElementById("filterType" + ucFirst(filterType)).className = 'filter';
 	document.getElementById("filterType" + ucFirst(t)).className = 'filterActive';
 	filterType = t;
+	filterUpdate();
 }
 
 function filterFixSet(t){
@@ -51,6 +100,7 @@ function filterFixSet(t){
 		heatmapLayerRemove();
 		heatmapLayerAdd();
 	}
+	filterUpdate();
 }
 
 function filterStationSet(s){
@@ -60,6 +110,19 @@ function filterStationSet(s){
 	if(heatmapLayerActive){
 		heatmapLayerRemove();
 		heatmapLayerAdd();
+	}
+	filterUpdate();
+}
+
+function filterStationToggle(s){
+	if(filterStation != s){
+		filterStationSet(s);
+	}
+	else{
+		if(heatmapLayerActive){
+			heatmapLayerRemove();
+		}
+		filterStationSet('all');
 	}
 }
 
@@ -71,13 +134,8 @@ function filterAltitudeSet(a){
 		heatmapLayerRemove();
 		heatmapLayerAdd();
 	}
+	filterUpdate();
 }
-
-/*function filterModelSet(m){
-	document.getElementById("filterStation" + ucFirst(filterModel)).className = 'filter';
-	document.getElementById("filterStation" + ucFirst(m)).className = 'filterActive';
-	filterModel = m;
-}*/
 
 function filterSourceSet(s){
 	document.getElementById("filterSource" + ucFirst(filterSource)).className = 'filter';
@@ -87,10 +145,40 @@ function filterSourceSet(s){
 		heatmapLayerRemove();
 		heatmapLayerAdd();
 	}
+	filterUpdate();
+}
+
+function filterNavSet(n){
+	document.getElementById("filterNav" + ucFirst(filterNav)).className = 'filter';
+	document.getElementById("filterNav" + ucFirst(n)).className = 'filterActive';
+	filterNav = n;
+	filterUpdate();
+	if(n!='all'){
+		filterSourceSet('adsb')
+	}
 }
 
 function filterSelectedSet(m){
 	document.getElementById("filterSelected" + ucFirst(filterSelected)).className = 'filter';
 	document.getElementById("filterSelected" + ucFirst(m)).className = 'filterActive';
 	filterSelected = m;
+	filterUpdate();
+}
+
+//Filter update wrapper
+function filterUpdate(){
+	//layersRefresh()
+	worker({ ships: true, planes: true, radiosondes: true, stations: true, stop: true  });
+
+	//indicator
+	if(document.getElementById("filterSelectedAll").className == 'filterActive' && document.getElementById("filterNavAll").className == 'filterActive' && document.getElementById("filterSourceAll").className == 'filterActive' && document.getElementById("filterStationAll").className == 'filterActive' && document.getElementById("filterTypeAll").className == 'filterActive' && document.getElementById("filterFixAll").className == 'filterActive' && /*document.getElementById("filterAltitude999999").className == 'filterAltitudeActive' && */filterAltitudeMin == 0 && filterAltitudeMax == 120000 && filterSpeedSliderMin.value == 0 && filterSpeedSliderMax.value ==2000){
+		document.getElementById("filterShow").className = 'button';
+	}
+	else{
+		document.getElementById("filterShow").className = 'buttonWait';
+	}
+	if(heatmapLayerActive){
+		heatmapLayerRemove();
+		heatmapLayerAdd();
+	}
 }
